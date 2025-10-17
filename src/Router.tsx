cc6a615e1893cc26ee5ccefc4ce09955806c2565
JanisSeibutis <janis.seibutis@gmail.com>
2025-10-17 21:30:00 +0200

    fix: set router base path for vite build

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,24 +4,29 @@ import { Home } from './pages/Home'
 import { Jobs } from './pages/Jobs'
 import { JobDetails } from './pages/JobDetails'
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Layout />,
 
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-      {
-        path: 'annonser',
-        element: <Jobs />,
-      },
-      {
-        path: 'annons/:id',
-        element: <JobDetails />,
-      },
-    ],
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+        {
+          path: 'annonser',
+          element: <Jobs />,
+        },
+        {
+          path: 'annons/:id',
+          element: <JobDetails />,
+        },
+      ],
+    },
+  ],
+  {
+    basename: '/Digi-design-system-with-taxonomy-api/',
   },
-])
+)
